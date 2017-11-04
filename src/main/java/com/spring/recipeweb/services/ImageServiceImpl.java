e8deab5a47d57b2f8922d0be79b05472f7135c45
Swastik Panda <swastiksuman@gmail.com>
2017-11-04 15:43:24 +0530

    added image functionality

---
+++ src/main/java/com/spring/recipeweb/services/ImageServiceImpl.java
@@ -0,0 +1,51 @@
+package com.spring.recipeweb.services;
+
+import java.io.IOException;
+
+import javax.transaction.Transactional;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.spring.recipeweb.domain.Recipe;
+import com.spring.recipeweb.repository.RecipeRepository;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+
+	private final RecipeRepository recipeRepository;
+
+	public ImageServiceImpl(RecipeRepository recipeService) {
+
+		this.recipeRepository = recipeService;
+	}
+
+	@Override
+	@Transactional
+	public void saveImageFile(Long recipeId, MultipartFile file) {
+
+		try {
+			Recipe recipe = recipeRepository.findById(recipeId).get();
+
+			Byte[] byteObjects = new Byte[file.getBytes().length];
+
+			int i = 0;
+
+			for (byte b : file.getBytes()) {
+				byteObjects[i++] = b;
+			}
+
+			recipe.setImage(byteObjects);
+
+			recipeRepository.save(recipe);
+		} catch (IOException e) {
+			// todo handle better
+			log.error("Error occurred", e);
+
+			e.printStackTrace();
+		}
+	}
+}
